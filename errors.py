b44900b93d535431829e5d016a419a1f8811c5d2
annes12345678910 <anneswidow@gmail.com>
2025-05-23 13:12:01 +0300

    Delete errors.py

--- errors.py
+++
@@ -1,8 +0,0 @@
-class PyppError(Exception):
-    """Base class for pygame++ errors"""
-    pass
-class AssetNotFoundError(PyppError):
-    """When an asset is not found (sound, image or smth)"""
-    def __init__(self, path, message="Failed to load Unknown asset"):
-        super().__init__(f"{message}: {path}")
-        self.path = path
